5a4b95d6a1100ab3914c102b1a72357a513d0ab9
axinger <axingrun@outlook.com>
2020-11-27 15:45:42 +0800

    update…

---
+++ test.py
@@ -0,0 +1,13 @@
+print("Python Hello, World!")
+
+list1 = ["a", "b", "c"]
+print(list1)
+list1.append('d')
+print(list1)
+# print('list = ',list)
+print('长度' + str(len(list1)))
+print('长度 = %d' % len(list1))
+
+number = 34
+
+print('这个数是: %d %s' % (number, "A"))
